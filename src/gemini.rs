1366532774c6a044f5d055975f0174f80aeed7e9
synoet <teonys@nyu.edu>
2024-10-22 17:52:24 -0400

    CHG move to use sse ?alt=sse

--- src/gemini.rs
+++ src/gemini.rs
@@ -2,7 +2,7 @@ use std::pin::Pin;
 
 use crate::{client::Client, error::GeminiError, types::content::GenerateContentErrorResponse};
 use futures::{stream::StreamExt, Stream};
-use serde_json::Value;
+use reqwest_eventsource::{Event, RequestBuilderExt};
 use tokio::sync::mpsc;
 
 use crate::types::{
@@ -14,20 +14,12 @@ pub struct Gemini<'c> {
     client: &'c Client,
 }
 
-#[derive(PartialEq)]
-enum ChunkType {
-    Start,
-    Comma,
-    End,
-}
-
 impl<'c> Gemini<'c> {
     pub fn new(client: &'c Client) -> Self {
         Self { client }
     }
 
     /// Creates a chat response
-    #[tracing::instrument(level = "debug", skip(self))]
     pub async fn generate_content(
         &self,
         model: GeminiModel,
@@ -72,7 +64,6 @@ impl<'c> Gemini<'c> {
 
     /// Create a chat stream response
     /// partial message deltas will be sent as stream chunks
-    #[tracing::instrument(level = "debug", skip(self))]
     pub async fn stream_generate_content(
         &self,
         model: GeminiModel,
@@ -81,7 +72,7 @@ impl<'c> Gemini<'c> {
         Pin<Box<dyn Stream<Item = Result<GenerateContentResponse, GeminiError>> + Send + 'static>>,
         GeminiError,
     > {
-        let url = format!("https://{}-aiplatform.googleapis.com/v1/projects/{}/locations/{}/publishers/google/models/{}:streamGenerateContent",
+        let url = format!("https://{}-aiplatform.googleapis.com/v1/projects/{}/locations/{}/publishers/google/models/{}:streamGenerateContent?alt=sse",
             self.client.config.location(),
             self.client.config.project_id(),
             self.client.config.location(),
@@ -99,161 +90,66 @@ impl<'c> Gemini<'c> {
         let api_key = token;
 
         tokio::spawn(async move {
-            let res = match client
+            let mut source = match client
                 .post(&url)
                 .header("content-type", "application/json; charset=utf-8")
                 .header("Authorization", format!("Bearer {}", api_key))
                 .json(&request)
-                .send()
-                .await
+                .eventsource()
             {
                 Ok(res) => res,
 
                 Err(e) => {
                     tracing::error!(error=?e, "failed to send request to google vertex");
-                    if let Err(send_error) = wx.send(Err(e.into())) {
-                        tracing::error!(error=?send_error, "failed to send error message to stream");
-                        return;
-                    }
                     return;
                 }
             };
 
-            tracing::debug!("response from google vertex received");
-
-            let mut stream = res.bytes_stream();
-            let mut pending_chunk: String = String::new();
-            while let Some(chunk) = stream.next().await {
-                let raw_chunk = match chunk {
-                    Ok(c) => c,
+            while let Some(sse_event) = source.next().await {
+                let event = match sse_event {
+                    Ok(e) => match e {
+                        Event::Message(m) => m,
+                        Event::Open => {
+                            tracing::trace!("received open event");
+                            continue;
+                        }
+                    },
                     Err(e) => {
                         tracing::error!(error=?e, "failed to read chunk from google vertex");
-                        if let Err(send_error) =
-                            wx.send(Err(GeminiError::ParseError(e.to_string())))
-                        {
-                            tracing::error!(error=?send_error, "failed to send error message to stream");
-                            break;
-                        }
                         return;
                     }
                 };
 
-                let mut chunk = match String::from_utf8(raw_chunk.to_vec()) {
+                let message = event.data;
+
+                let res = match serde_json::from_str::<GenerateContentResponse>(&message) {
                     Ok(c) => c,
-                    Err(e) => {
-                        tracing::error!(error=?e, "failed to parse chunk while streaming from google vertex");
-                        if let Err(send_error) =
-                            wx.send(Err(GeminiError::ParseError(e.to_string())))
-                        {
-                            tracing::error!(error=?send_error, "failed to send error message to stream");
-                            break;
-                        }
-                        return;
-                    }
-                };
-
-                tracing::debug!("received chunk {}", &chunk);
-
-                if !pending_chunk.is_empty() {
-                    tracing::debug!("appending pending chunk to new chunk");
-                    chunk = pending_chunk.clone() + &chunk;
-                }
-
-                let mut lines: Vec<String> =
-                    chunk.lines().map(|c| c.to_string()).collect::<Vec<_>>();
-
-                // An error message will be a single chunk with both a start and end token
-                // need to extract the inner json object with error message and try and parse it
-                if lines[0].as_str().starts_with("[")
-                    && lines[lines.len() - 1].as_str().ends_with("]")
-                {
-                    let error_content = lines
-                        .join("")
-                        .trim_matches(|c| c == '[' || c == ']')
-                        .trim()
-                        .to_string();
-
-                    match serde_json::from_str::<GenerateContentErrorResponse>(&error_content) {
-                        Ok(c) => {
-                            if let Err(send_error) = wx.send(Err(c.into())) {
-                                tracing::error!(
-                                    error=?send_error,
-                                    "failed to send error message to stream"
-                                );
-                                break;
+                    Err(parse_error) => {
+                        // Check if the message is an error message
+                        match serde_json::from_str::<GenerateContentErrorResponse>(&message) {
+                            Ok(c) => {
+                                tracing::error!(error=?c, "generate content failed");
+                                if let Err(send_error) = wx.send(Err(c.into())) {
+                                    tracing::error!(
+                                        error=?send_error,
+                                        "failed to send error message to stream"
+                                    );
+                                    break;
+                                }
+                                return;
                             }
-                            return;
-                        }
-                        Err(e) => {
-                            if let Err(send_error) = wx.send(Err(GeminiError::ParseError(format!(
-                                "failed to parse error message: {}",
-                                e
-                            )))) {
-                                tracing::error!(
-                                    error=?send_error,
-                                    "failed to send error message to stream"
-                                );
-                                break;
+                            // this is not an error message
+                            Err(_) => {
+                                tracing::error!(error=?parse_error, "failed to parse response from google vertex");
+                                if let Err(send_error) =
+                                    wx.send(Err(GeminiError::ParseError(parse_error.to_string())))
+                                {
+                                    tracing::error!(error=?send_error, "failed to send error message to stream");
+                                    break;
+                                }
+                                return;
                             }
-                            return;
-                        }
-                    };
-                }
-
-                let chunk_type = match lines[0].as_str() {
-                    "[{" => {
-                        tracing::debug!("received start token in chunk");
-                        let mut new_line = lines.remove(0).to_string();
-                        new_line = new_line[1..new_line.len()].to_string();
-                        lines.insert(0, new_line);
-                        pending_chunk = String::new();
-                        ChunkType::Start
-                    }
-                    "," => {
-                        tracing::debug!("received continue token in chunk");
-                        lines.remove(0);
-                        pending_chunk = String::new();
-                        ChunkType::Comma
-                    }
-                    "]" => {
-                        tracing::debug!("received end token in chunk");
-                        ChunkType::End
-                    }
-                    _ => {
-                        continue;
-                    }
-                };
-
-                if chunk_type == ChunkType::End {
-                    break;
-                }
-
-                let content = lines.join("");
-
-                // If the chunk ends with a comma, we need to check if the chunk is valid json
-                // If it is, we parse it as json and continue to the next chunk
-                // If it is not, we wait for the next chunk and combine them together
-                if chunk_type == ChunkType::Comma {
-                    if let Err(parse_error) = serde_json::from_str::<Value>(content.as_str()) {
-                        tracing::debug!(error=?parse_error, "could not parse chunk as json, setting as pending chunk");
-                        pending_chunk.push_str(content.as_str());
-                        continue;
-                    } else {
-                        pending_chunk = String::new();
-                    }
-                }
-
-                let res = match serde_json::from_str::<GenerateContentResponse>(&content) {
-                    Ok(c) => c,
-                    Err(e) => {
-                        tracing::error!(error=?e, "failed to parse response from google vertex");
-                        if let Err(send_error) =
-                            wx.send(Err(GeminiError::ParseError(e.to_string())))
-                        {
-                            tracing::error!(error=?send_error, "failed to send error message to stream");
-                            break;
-                        }
-                        return;
+                        };
                     }
                 };
 
